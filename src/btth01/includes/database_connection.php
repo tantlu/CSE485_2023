50b55058f18473bb9232da763f96335a0cae3312
NguyenHieuHT <105448229+NguyenHieuHT@users.noreply.github.com>
2023-02-23 14:19:33 +0700

    Add files via upload

--- src/btth01/includes/database_connection.php
+++ src/btth01/includes/database_connection.php
@@ -5,14 +5,3 @@
         die('Kết nối tới Server lỗi');
     }
 ?>
-<?php
-
-$sql = "SELECT * FROM baiviet ORDER BY ngayviet DESC LIMIT 4";
-$result = mysqli_query($conn, $sql);
-if(mysqli_num_rows($result) > 0){
-    while($row = mysqli_fetch_assoc($result)){
-?>
-<?php
-    }
- }
-?>
