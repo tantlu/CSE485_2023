c378c5fe490c1988d5fe3f912e8d172930aa0258
tendz <nguyentan7799@gmail.com>
2023-02-26 10:04:35 +0700

    Update header.php
    fix logo

--- src/btth01/includes/header.php
+++ src/btth01/includes/header.php
@@ -13,7 +13,7 @@
             <div class="container-fluid">
                 <div class="my-logo">
                     <a class="navbar-brand" href="#">
-                        <img src="images/logo2.png" alt="" class="img-fluid">
+                        <img src="images/logo2.png" alt="" class="img-fluid" style="mix-blend-mode: color-burn">
                     </a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
